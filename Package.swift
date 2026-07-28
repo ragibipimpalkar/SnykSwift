504b01c4b2c2383f29b2bc28e9ac792c0adba8b4
Ragini pimpalkar <119956625+ragibipimpalkar@users.noreply.github.com>
2026-07-28 23:42:36 +0530

    commit 2 for package.swift

--- Package.swift
+++ Package.swift
@@ -1,23 +1,25 @@
 // swift-tools-version:5.7
- 
+
 import PackageDescription
- 
+
 let package = Package(
     name: "SnykSwiftTest",
-    platforms: [
-        .iOS(.v14),
-        .macOS(.v12)
+    products: [
+        .library(
+            name: "SnykSwiftTest",
+            targets: ["SnykSwiftTest"]
+        )
     ],
     dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.6.0"),
-        .package(url: "https://github.com/realm/SwiftLint.git", from: "0.50.0")
+        .package(
+            url: "https://github.com/Alamofire/Alamofire.git",
+            from: "5.6.0"
+        )
     ],
     targets: [
         .target(
             name: "SnykSwiftTest",
-            dependencies: [
-                "Alamofire"
-            ]
+            dependencies: ["Alamofire"]
         )
     ]
 )
